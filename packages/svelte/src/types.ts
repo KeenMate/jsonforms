7cc6a45fd9378df36789bddfad23c09d3c99bc35
Filip Jakab <jakabfilip.fj@gmail.com>
2025-02-26 02:32:00 +0100

    wip(svelte): new svelte package
    Copied vue package and started rewriting it to svelte

---
+++ packages/svelte/src/types.ts
@@ -0,0 +1,14 @@
+export type JsonFormsContext = {
+	jsonForms: unknown
+	jsonFormsDispatch: unknown
+}
+
+export type DispatchCellProps = {
+	schema: unknown
+	uischema: unknown
+	path: unknown
+	enabled: unknown
+	renderers: unknown
+	cells: unknown
+	config: unknown
+}
